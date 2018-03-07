d6281021e8a48353a214ea203e65021cb778deb2
zhaorui <zhaorui@qianshengqian.com>
2018-03-07 20:54:34 +0800

    项目去除urule部分

--- src/main/java/com/yf/rule/builtinaction/CustomMathAction.java
+++ src/main/java/com/yf/rule/builtinaction/CustomMathAction.java
@@ -6,10 +6,17 @@ import com.bstek.urule.model.library.action.annotation.ActionMethodParameter;
 import com.bstek.urule.runtime.builtinaction.MathAction;
 
 @ActionBean(name="自定义数学函数")
-public class CustomMathAction extends MathAction{
+public class CustomMathAction{
 	@ActionMethod(name="取模")
     @ActionMethodParameter(names={"目标数字", "取模数"})
 	public Integer mod(Integer a, Integer b){
 		return a % b; 
+	}
+	
+	@ActionMethod(name="睡眠")
+    @ActionMethodParameter(names={"名称", "睡眠毫秒时间"})
+	public String exec(String name, Long sleep) throws InterruptedException{
+		Thread.sleep(sleep);
+		return name + "=" + sleep;
 	}
 }
